7a6d199e4edd74428898aaca025e9bf0144bf737
Michael Romay <amerror@gmail.com>
2020-05-14 14:36:57 -0700

    wrote getMethod and getBody selector

--- src/selectors/restySelector.js
+++ src/selectors/restySelector.js
@@ -1,1 +1,22 @@
 export const getURL = state => state.url;
+
+export const getMethod = state => state.method;
+
+export const getBody = state => state.body;
+
+
+
+// case 'SET_URL':
+//   return { ...state, url: action.payload };
+// case 'SET_METHOD':
+//   return { ...state, method: action.payload };
+// case 'SET_BODY':
+//   return { ...state, body: action.payload };
+// case 'SET_REQUESTS':
+//   return { ...state, requests: action.payload };
+// case 'SET_DISABLE':
+//   return { ...state, disable: action.payload };
+// case 'SET_RESPONSE':
+//   return { ...state, response: action.payload };
+// case 'SET_HEADERS':
+//   return { ...state, headers: action.payload };
