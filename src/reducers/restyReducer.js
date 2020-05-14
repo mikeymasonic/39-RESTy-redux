ae0cd97b610cbfb76e7bd7c97b8f0650f649c423
Michael Romay <amerror@gmail.com>
2020-05-14 14:29:07 -0700

    wrote SET_HEADERS case for restyReducer

--- src/reducers/restyReducer.js
+++ src/reducers/restyReducer.js
@@ -1,4 +1,4 @@
-import { SET_URL, SET_METHOD, SET_BODY, SET_REQUESTS, SET_DISABLE, SET_RESPONSE } from '../actions/restyActions';
+import { SET_URL, SET_METHOD, SET_BODY, SET_REQUESTS, SET_DISABLE, SET_RESPONSE, SET_HEADERS } from '../actions/restyActions';
 
 export default function reducer(state, action) {
   switch(action.type) {
@@ -14,27 +14,9 @@ export default function reducer(state, action) {
       return { ...state, method: action.payload };
     case SET_RESPONSE:
       return { ...state, method: action.payload };
+    case SET_HEADERS:
+      return { ...state, method: action.payload };
+    default:
+      return state;
   }
 }
-
-// export function reducer(state, action) {
-//   switch(action.type) {
-//     case 'SET_URL':
-//       return { ...state, url: action.payload };
-//     case 'SET_METHOD':
-//       return { ...state, method: action.payload };
-//     case 'SET_BODY':
-//       return { ...state, body: action.payload };
-//     case 'SET_REQUESTS':
-//       return { ...state, requests: action.payload };
-//     case 'SET_DISABLE':
-//       return { ...state, disable: action.payload };
-//     case 'SET_RESPONSE':
-//       return { ...state, response: action.payload };
-//     case 'SET_HEADERS':
-//       return { ...state, headers: action.payload };
-    
-//     default:
-//       return state;
-//   }
-// }
